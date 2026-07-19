34054d661024417d4f36b035164a32d29b0da20b
rishi <rishirk253@gamil.com>
2026-07-19 14:17:17 +0530

    added crud only update is pending

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import Auth from "./config/components/auth";
+import Crud from "./config/components/Crud";
 
 function App(){
 
   return <div>
     <Auth/>
-
+    <Crud/>
 
   </div>
 }
---
+++ src/config/components/Crud.jsx
@@ -0,0 +1,128 @@
+import { useEffect, useState } from "react"
+import { db } from "../firebase.config";
+import { 
+    getDocs,
+    collection, 
+    addDoc, 
+    deleteDoc, 
+    doc,
+    onSnapshot
+} from "firebase/firestore";
+
+export default function Crud(){
+
+    const [books,setBooks] = useState([]);
+
+    const [newBook,setNewBook] = useState("");
+    const [newDescription,setNewDescription] = useState("");
+    const [newQuantity,setNewQuantity] = useState(0);
+
+    useEffect(() => {
+    // This listens to live updates in Firestore directly
+    const unsubscribe = onSnapshot(booksCollection, (snapshot) => {
+        const filteredData = snapshot.docs.map((doc) => ({
+            ...doc.data(),
+            id: doc.id
+        }));
+        setBooks(filteredData);
+    });
+
+    // Clean up the listener when the component unmounts
+    return () => unsubscribe();
+}, []);    
+     async function getList(){
+            try{
+                const data = await getDocs(booksCollection);
+                
+                const filteredData = data.docs.map((doc) => ({
+                    ...doc.data(),
+                    id: doc.id
+                }));
+
+                setBooks(filteredData);
+                   
+            }
+            catch(err){
+                console.log(err);
+            }
+        };
+    const booksCollection = collection(db,"books");
+    useEffect(()=>{
+       
+        getList();
+    
+    },[]);
+
+    async function addBook(){
+        try{
+            await addDoc(booksCollection, {
+                bookName: newBook, description: newDescription,
+                quantity: newQuantity
+            });
+
+            getList();
+        
+        }catch(err){
+            console.log(err);
+        }
+    }
+
+    async function deleteBook(id){
+        
+        try{
+            const bookDoc = doc(db,"books",id);
+            await deleteDoc(bookDoc);
+        }
+        catch(err){
+            console.log(err);
+        }
+
+    }
+    return <>
+
+       <div className="container my-6">
+         {books.map((m)=> <p key={m.id}>
+         
+            {m.bookName + " "}
+
+         <button className="btn btn-danger"
+         onClick={()=>deleteBook(m.id)}>
+         
+            delete
+         
+         </button>
+         
+         </p>)}
+        
+       </div>
+
+
+      <div className="container">
+            <p>
+                <input type="text" name="" id="" placeholder="book name"
+                onChange={(e)=>setNewBook(e.target.value)}/>
+            </p>
+            <p>
+                <input type="text" 
+                placeholder="description"
+                onChange={(e)=>setNewDescription(e.target.value)}
+                />
+            </p>
+            <p>
+                <input type="number" 
+                placeholder="quantity"
+                onChange={(e)=>setNewQuantity(e.target.valueAsNumber)}
+            />
+            </p>
+            <button onClick={addBook}
+            className="btn btn-success">
+                add
+            </button>
+
+      </div>
+
+
+
+
+    </>
+}
